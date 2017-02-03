0b42880258d34386fcb4c06afd9e5196a7cb1f7a
Israel Manzo <israman814@gmail.com>
2017-02-03 13:57:10 -0500

    updating appdelegate.swift with persistance firebase

--- iOrganized/iOrganized/AppDelegate.swift
+++ iOrganized/iOrganized/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseDatabase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FIRApp.configure()
+        FIRDatabase.database().persistenceEnabled = true
         // Override point for customization after application launch.
         return true
     }
