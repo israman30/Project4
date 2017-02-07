7e1501adb6c67841a388a8e96e7b4377b977d5c2
Israel Manzo <israman814@gmail.com>
2017-02-07 18:45:17 -0500

    Adding google maps

--- iOrganized/iOrganized/GoogleMapViewController.swift
+++ iOrganized/iOrganized/GoogleMapViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 
-class GoogleMapViewController: UIViewController {
+class GoogleMapViewController: UIViewController,CLLocationManagerDelegate, GMSMapViewDelegate {
 
     @IBOutlet weak var googleMapView: GMSMapView!
+    
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.startMonitoringSignificantLocationChanges()
+        
         initGoogleMaps()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     func initGoogleMaps(){
@@ -27,14 +35,52 @@ class GoogleMapViewController: UIViewController {
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
-        googleMapView = mapView
+        self.googleMapView.camera =  camera
+        
+        self.googleMapView.delegate = self
+        self.googleMapView.isMyLocationEnabled = true
+        self.googleMapView.settings.myLocationButton = true
+        
         
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         marker.title = "Sydney"
         marker.snippet = "Australia"
         marker.map = mapView
-    
+        
         
     }
+    
+    //MARK: Location manager delegate functions
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while get location \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!, zoom: 17.0)
+        
+        self.googleMapView.animate(to: camera)
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    //MARK: GMSMapView delegate
+    
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+        self.googleMapView.isMyLocationEnabled = true
+    }
+    
+    func mapView(_ mapView: GMSMapView, willMove gesture: Bool) {
+        self.googleMapView.isMyLocationEnabled = true
+        if (gesture){
+            mapView.selectedMarker = nil
+        }
+    }
 }
+
+
+
+
+
+
