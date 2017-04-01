7ea9b93421100ad915efc16b1129aeb2d46f7167
Israel Manzo <israman814@gmail.com>
2017-04-01 19:51:30 -0400

    Updating text field

---
+++ iOrganized/iOrganized/TextFieldEffects.h
@@ -0,0 +1,18 @@
+//
+//  TextFieldEffects.h
+//  iOrganized
+//
+//  Created by Israel Manzo on 4/1/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TextFieldEffects.
+FOUNDATION_EXPORT double TextFieldEffectsVersionNumber;
+
+//! Project version string for TextFieldEffects.
+FOUNDATION_EXPORT const unsigned char TextFieldEffectsVersionString[];
+
+// In this header, you should import all the open headers of your framework using statements like #import <TextFieldEffects/PublicHeader.h>
+
